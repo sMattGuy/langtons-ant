cbdc48e1b41f14b45baf406bf2b2e6fbc7f565b2
MattGuy2 <matthewflammia@aol.com>
2024-04-10 12:51:19 -0400

    Updated code

--- script.js
+++ script.js
@@ -12,13 +12,17 @@ class Ant{
 		this.yPos = yPos;
 	}
 }
-let ant;
+const ant_array = new Array();
+let ANT_COUNT = 1;
 //sliders
-//boid modifiers
+//ant modifiers
 let speedSlider = document.getElementById("maxSpeed");
 let speedInfo = document.getElementById("speedDisplay");
 speedInfo.innerHTML = speedSlider.value;
 
+let antSlider = document.getElementById("maxAnts");
+let antInfo = document.getElementById("antDisplay");
+antInfo.innerHTML = antSlider.value;
 //canvas settings
 let widthSlider = document.getElementById("maxWidth");
 let widthInfo = document.getElementById("widthDisplay");
@@ -26,13 +30,18 @@ widthInfo.innerHTML = widthSlider.value;
 let heightSlider = document.getElementById("maxHeight");
 let heightInfo = document.getElementById("heightDisplay");
 heightInfo.innerHTML = heightSlider.value;
+let tileSlider = document.getElementById("maxTile");
+let tileInfo = document.getElementById("tileDisplay");
+tileInfo.innerHTML = tileSlider.value;
 
 //buttons
 let startButton = document.getElementById("startGame");
 let clearButton = document.getElementById("drawClear");
+let gridButton = document.getElementById("showGrid");
 
 let iteration = 0;
 //constants
+let draw_grid = true;
 let START = false;
 let SPEED = speedSlider.value;
 //canvas constants
@@ -44,16 +53,13 @@ let TILESIZE = 10;
 
 let drawArray = new Array(Math.floor(FIELDX/TILESIZE));
 
-//canvas listen to get X and Y of mouse click to place flag
-canvas.addEventListener('mousedown', e => {
-		drawArray[ant.xPos][ant.yPos].ant = 0;
-		ant.xPos = Math.floor(e.offsetX/TILESIZE);
-		ant.yPos = Math.floor(e.offsetY/TILESIZE);
-		drawArray[ant.xPos][ant.yPos].ant = 1;
-});
 speedSlider.oninput = function(){
 	SPEED = parseInt(this.value);
 	speedInfo.innerHTML = this.value;
+}
+antSlider.oninput = function(){
+	ANT_COUNT = parseInt(this.value);
+	antInfo.innerHTML = this.value;
 }
 
 //canvas sliders
@@ -69,8 +75,16 @@ heightSlider.oninput = function(){
 	heightInfo.innerHTML = this.value;
 	createArray();
 }
+tileSlider.oninput = function(){
+	TILESIZE = parseInt(this.value);
+	tileInfo.innerHTML = this.value;
+	createArray();
+}
 startButton.oninput = function(){
 	START = !START;
+}
+gridButton.oninput = function(){
+	draw_grid = !draw_grid;
 }
 clearButton.onclick = function(){
 	createArray();
@@ -78,42 +92,52 @@ clearButton.onclick = function(){
 function createArray(){
 	drawArray = new Array(Math.floor(FIELDX/TILESIZE));
 	for(let i=0;i<Math.floor(FIELDX/TILESIZE);i++){
-		drawArray[i] = new Array(FIELDY/TILESIZE);
+		drawArray[i] = new Array(Math.floor(FIELDY/TILESIZE));
 		for(let j=0;j<Math.floor(FIELDY/TILESIZE);j++){
 			drawArray[i][j] = {'ant':0,'color':0};
 		}
 	}
 	iteration = 0;
-	ant.xPos = Math.floor(Math.floor(FIELDX/TILESIZE)/2);
-	ant.yPos = Math.floor(Math.floor(FIELDY/TILESIZE)/2);
-	drawArray[ant.xPos][ant.yPos] = {'ant':1,'color':0};
+	for(let i=0;i<ant_array.length;i++){
+		ant_array[i].xPos = Math.floor(Math.floor(FIELDX/TILESIZE)/2);
+		ant_array[i].yPos = Math.floor(Math.floor(FIELDY/TILESIZE)/2);
+		drawArray[ant_array[i].xPos][ant_array[i].yPos] = {'ant':1,'color':0};
+	}
 }
 
 //frames
-var frames = {
-	speed: 17,
-	max: -1,
-	timer: '',
-	run: function (func) {
-		this.timer = setInterval(func, this.speed);
-	},
-	start: function (func, speed = 17) {
-		this.speed = speed;
-		this.run(func);
-	}
-}
-//this is what loops the frames indefinietly
-async function doFrames() {
-	let speedCount = 0;
-	frames.start(() => {
-		draw();
-		if(START && speedCount >= SPEED){
-			updateGrid();
-			iteration++;
-			speedCount = 0;
+let speedCount = 0;
+function doFrames() {
+	draw();
+	let current_ants = ANT_COUNT;
+	if(current_ants != ant_array.length){
+		let ant_diff = current_ants - ant_array.length;
+		if(ant_diff > 0){
+			// add ants
+			while(ant_diff != 0){
+				let new_x = Math.floor(Math.random()*(FIELDX/TILESIZE));
+				let new_y = Math.floor(Math.random()*(FIELDY/TILESIZE));
+				ant_array.push(new Ant(new_x, new_y));
+				drawArray[new_x][new_y].ant = 1;
+				ant_diff--;
+			}
 		}
-		speedCount++;
-	}, frames.speed);
+		else{
+			// remove ants
+			while(ant_diff != 0){
+				let dead_ant = ant_array.pop();
+				drawArray[dead_ant.xPos][dead_ant.yPos].ant = 0;
+				ant_diff++;
+			}
+		}
+	}
+	if(START && speedCount >= SPEED){
+		updateGrid();
+		iteration++;
+		speedCount = 0;
+	}
+	speedCount++;
+	window.requestAnimationFrame(doFrames);
 }
 
 /*
@@ -121,7 +145,7 @@ async function doFrames() {
 	it creates the initial units and triggers the frames
 */
 function init(){
-	ant = new Ant(25,25);
+	ant_array.push(new Ant(25,25));
 	createArray();
 	doFrames();
 }
@@ -134,14 +158,14 @@ function draw(){
 	ctx.fillStyle = '#eee';
 	ctx.fillRect(0,0,FIELDX,FIELDY);
 	//draw gridlines
-	ctx.strokeStyle = 'rgba(25,25,25,0.03)';
-	ctx.beginPath();
-	for(let i=0;i<Math.floor(FIELDX/TILESIZE);i++){
-		for(let j=0;j<Math.floor(FIELDY/TILESIZE);j++){
-			ctx.rect(i*TILESIZE,j*TILESIZE,TILESIZE,TILESIZE);
+	if(draw_grid && TILESIZE >=5){
+		ctx.strokeStyle = 'rgba(25,25,25,0.1)';
+		for(let i=0;i<Math.floor(FIELDX/TILESIZE);i++){
+			for(let j=0;j<Math.floor(FIELDY/TILESIZE);j++){
+				ctx.strokeRect(i*TILESIZE,j*TILESIZE,TILESIZE,TILESIZE);
+			}
 		}
 	}
-	ctx.stroke();
 	//draw array
 	for(let i=0;i<Math.floor(FIELDX/TILESIZE);i++){
 		for(let j=0;j<Math.floor(FIELDY/TILESIZE);j++){
@@ -150,8 +174,8 @@ function draw(){
 				ctx.fillRect(i*TILESIZE,j*TILESIZE,TILESIZE,TILESIZE);
 			}
 			if(drawArray[i][j].ant == 1){
-				ctx.fillStyle = "red";
-				ctx.fillRect(i*TILESIZE+3,j*TILESIZE+3,TILESIZE/2,TILESIZE/2);
+				ctx.fillStyle = "rgba(255,0,0,.5)";
+				ctx.fillRect(i*TILESIZE,j*TILESIZE,TILESIZE,TILESIZE);
 			}
 		}
 	}
@@ -160,36 +184,38 @@ function draw(){
 	ctx.fillText(`Step:${iteration}`,0,10);
 }
 function updateGrid(){
-	if(drawArray[ant.xPos][ant.yPos].color == 0){
-		//white square
-		//turn 90d clock wise flip color, move forward
-		//set current tile to antless
-		drawArray[ant.xPos][ant.yPos].ant = 0;
-		drawArray[ant.xPos][ant.yPos].color = (drawArray[ant.xPos][ant.yPos].color + 1)%2;
-		//turn ant
-		ant.facing = (ant.facing + 1)%4;
-		//move forward
-		moveAnt();
-		//set new ant tile
-		drawArray[ant.xPos][ant.yPos].ant = 1;
-	}
-	else{
-		//black square
-		//turn 90d CC flip and move
-		//set current tile to antless
-		drawArray[ant.xPos][ant.yPos].ant = 0;
-		drawArray[ant.xPos][ant.yPos].color = (drawArray[ant.xPos][ant.yPos].color + 1)%2;
-		//turn ant
-		ant.facing = ant.facing - 1;
-		if(ant.facing < 0)
-			ant.facing = 3;
-		//move forward
-		moveAnt();
-		//set new ant tile
-		drawArray[ant.xPos][ant.yPos].ant = 1;
+	for(let i=0;i<ant_array.length;i++){
+		if(drawArray[ant_array[i].xPos][ant_array[i].yPos].color == 0){
+			//white square
+			//turn 90d clock wise flip color, move forward
+			//set current tile to antless
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].ant = 0;
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].color = (drawArray[ant_array[i].xPos][ant_array[i].yPos].color + 1)%2;
+			//turn ant
+			ant_array[i].facing = (ant_array[i].facing + 1)%4;
+			//move forward
+			moveAnt(ant_array[i]);
+			//set new ant tile
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].ant = 1;
+		}
+		else{
+			//black square
+			//turn 90d CC flip and move
+			//set current tile to antless
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].ant = 0;
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].color = (drawArray[ant_array[i].xPos][ant_array[i].yPos].color + 1)%2;
+			//turn ant
+			ant_array[i].facing = ant_array[i].facing - 1;
+			if(ant_array[i].facing < 0)
+				ant_array[i].facing = 3;
+			//move forward
+			moveAnt(ant_array[i]);
+			//set new ant tile
+			drawArray[ant_array[i].xPos][ant_array[i].yPos].ant = 1;
+		}
 	}
 }
-function moveAnt(){
+function moveAnt(ant){
 	if(ant.facing == 0){
 		//north
 		ant.yPos--;
